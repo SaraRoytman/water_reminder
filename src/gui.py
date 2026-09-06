53191011abf1f17741170902a4adfbdc6a181720
sara2 <sara270498@gmail.com>
2026-09-06 15:55:36 +0300

    update

--- src/gui.py
+++ src/gui.py
@@ -12,14 +12,22 @@ class WaterReminderGUI:
         ui.timer(120, self.show_window, once=True)
 
     def hide_window(self):
-        app.native.main_window.hide()
-
+        if app.native.main_window:
+            app.native.main_window.hide()
+        else:
+            self.label.set_visibility(False)
+            self.drink_btn.set_visibility(False)
+        
     def show_window(self):
-        app.native.main_window.show()
+        if app.native.main_window:
+            app.native.main_window.show()
+        else:
+            self.label.set_visibility(True)
+            self.drink_btn.set_visibility(True)   
 
     def on_drink(self):
         self.hide_window()
-        ui.timer(60, self.show_window, once=True)
+        ui.timer(120, self.show_window, once=True)
         
 
 
