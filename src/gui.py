013779fbc9b06eb8c5fde6122e9e4f60cf9b46fc
sara2 <sara270498@gmail.com>
2026-09-06 12:14:26 +0300

    added things

--- src/gui.py
+++ src/gui.py
@@ -1,26 +1,31 @@
 import tkinter as tk
 
-class watergui:
+class WaterReminderGUI:
     def __init__(self, root):
         self.root = root
         self.root.geometry("400x300")
-
         self.root.attributes('-topmost', True)
-        self.label = tk.Label(root, text="Drink Water!", font=("Arial", 28, "bold"), fg="green")
-        self.label.pack()
+        
+        self.label = tk.Label(root, text="Drink Water!", font=("Arial", 28, "bold"), fg="blue")
+        self.label.pack(pady=30) 
 
         self.drink_btn = tk.Button(root, text="שתיתי מים!", font=("Arial", 16), command=self.on_drink)
         self.drink_btn.pack(pady=10)
 
+        self.quit_btn = tk.Button(root, text="סגור תזכורת לחלוטין", font=("Arial", 12), fg="red", command=self.root.destroy)
+        self.quit_btn.pack(pady=10)
+
         self.root.withdraw()
-        self.root.after(5000, self.show_window)
+        
+        self.root.after(120000, self.show_window) 
 
     def show_window(self):
         self.root.deiconify()
 
     def on_drink(self):
         self.root.withdraw()
-        self.root.after(25000, self.show_window)
+        
+        self.root.after(120000, self.show_window)
         
 
 
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
-from gui import watergui
+from gui import WaterReminderGUI
 
 def main():
     root = tk.Tk()
-    app = watergui(root)
+    app = WaterReminderGUI(root)
     root.mainloop()
 
 if __name__ == "__main__":
