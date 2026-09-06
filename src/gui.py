73c8de874f28209ac33fea0f00b82568d8e48292
sara2 <sara270498@gmail.com>
2026-09-06 11:33:16 +0300

    popping window

--- src/gui.py
+++ src/gui.py
@@ -4,8 +4,23 @@ class watergui:
     def __init__(self, root):
         self.root = root
         self.root.geometry("400x300")
+
+        self.root.attributes('-topmost', True)
         self.label = tk.Label(root, text="Drink Water!", font=("Arial", 28, "bold"), fg="green")
         self.label.pack()
+
+        self.drink_btn = tk.Button(root, text="שתיתי מים!", font=("Arial", 16), command=self.on_drink)
+        self.drink_btn.pack(pady=10)
+
+        self.root.withdraw()
+        self.root.after(5000, self.show_window)
+
+    def show_window(self):
+        self.root.deiconify()
+
+    def on_drink(self):
+        self.root.withdraw()
+        self.root.after(25000, self.show_window)
         
 
 
