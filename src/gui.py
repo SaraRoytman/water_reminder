5d30cb5c26adcf3cce6d3ebf558effa5cab93e0f
sara2 <sara270498@gmail.com>
2026-09-09 11:24:53 +0300

    working on the logic

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,6 @@
 
 from nicegui import ui, app
+from logic import WaterTracker
 
 class WaterReminderGUI:
     def __init__(self):
@@ -8,11 +9,16 @@ class WaterReminderGUI:
         app.native.window_args['height'] = 350
 
         ui.query('body').classes('bg-stone-700 overflow-hidden')
+
+        self.tracker = WaterTracker()
         
         with ui.column().classes('w-full h-screen items-center justify-center gap-6'):
             self.label = ui.label("Drink Water!").classes('text-red-500 text-2xl font-semibold')
-            self.drink_btn = ui.button("!שתיתי כבר", on_click=self.on_drink, color='light-blue-4').classes('font-mono font-bold text-lg text-black')
-
+            self.total_label = ui.label(f"{self.tracker.consumed} ml today").classes('text-white text-md font-mono')
+            
+            self.drink_btn = ui.button("500 מ״ל", on_click=lambda: self.on_drink(self.tracker.add_tub0), color='light-blue-4').classes('font-mono font-bold text-lg text-black')
+            self.drink_btn_250 = ui.button("250 מ״ל", on_click=lambda: self.on_drink(self.tracker.add_tub1), color='light-blue-4').classes('font-mono font-bold text-lg text-black')
+            self.tmp_btn = ui.button(on_click=self.hide_window, color='white').classes('absolute right-2 top-1/2 -translate-y-1/2')
 
             
         ui.timer(120, self.show_window, once=True)
@@ -31,8 +37,12 @@ class WaterReminderGUI:
             self.label.set_visibility(True)
             self.drink_btn.set_visibility(True)   
 
-    def on_drink(self):
-        self.hide_window()
+    def on_drink(self, add_func):
+
+        add_func()
+        self.total_label.set_text(f"{self.tracker.consumed} ml today")
+        
+        ui.timer(10, self.hide_window, once=True)
         ui.timer(360, self.show_window, once=True)
 
 
---
+++ src/logic.py
@@ -0,0 +1,11 @@
+class WaterTracker:
+    def __init__(self):
+        self.consumed = 0
+
+    def add_tub0(self, amount = 500):
+        self.consumed += amount
+        return self.consumed
+
+    def add_tub1(self, amount = 250):
+        self.consumed += amount
+        return self.consumed
