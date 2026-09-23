8f1597641cf21cae2e77e17514524fe1266772a2
sara2 <sara270498@gmail.com>
2026-09-23 18:30:18 +0300

    update

--- src/gui.py
+++ src/gui.py
@@ -14,50 +14,52 @@ class WaterReminderGUI:
 
         ui.query('body').classes('bg-stone-700 overflow-hidden')
 
-        self.selection_container = ui.column().classes('w-full h-screen items-center justify-center gap-6')
-        self.tracker_container = ui.column().classes('w-full h-screen items-center justify-center gap-6')
+        self.selection_box = ui.column().classes('w-full h-screen items-center justify-center gap-6')
+        self.tracker_box = ui.column().classes('w-full h-screen items-center justify-center gap-6')
 
-        self.tracker_container.set_visibility(False)
+        self.tracker_box.set_visibility(False)
 
-        with self.selection_container:
+        with self.selection_box:
             self.selection_screen = user_select_screen(on_select=self.on_user_selected)
 
-        with self.tracker_container:
+        with self.tracker_box:
             self.tracker_screen = Main_tracker_screen(on_back=self.show_selection_screen, on_finish=self.hide_window)
 
         if app.native.main_window:
-            self.selection_container.set_visibility(False)
+            self.selection_box.set_visibility(False)
             self.tracker_screen.load_user('Developer')
-            self.tracker_container.set_visibility(True)
+            self.tracker_box.set_visibility(True)
         else:
-            self.tracker_container.set_visibility(False)
-            self.selection_container.set_visibility(True)
+            self.tracker_box.set_visibility(False)
+            self.selection_box.set_visibility(True)
 
         ui.timer(360, self.show_window)
 
     def on_user_selected(self, user_type):
-        self.selection_container.set_visibility(False)
+        self.selection_box.set_visibility(False)
         self.tracker_screen.load_user(user_type)
-        self.tracker_container.set_visibility(True)
+        self.tracker_box.set_visibility(True)
         
     def show_selection_screen(self):
-        self.tracker_container.set_visibility(False)
-        self.selection_container.set_visibility(True)
+        if not app.native.main_window:
+            self.tracker_box.set_visibility(False)
+            self.selection_box.set_visibility(True)       
 
     def hide_window(self):
-        self.show_selection_screen()
         if app.native.main_window:
-           app.native.main_window.hide()
+            app.native.main_window.hide()
         else:
-            self.selection_container.set_visibility(False)
-            self.tracker_container.set_visibility(False)
+            self.show_selection_screen()
+            self.selection_box.set_visibility(False)
+            self.tracker_box.set_visibility(False)
         
     def show_window(self):
         if app.native.main_window:
             app.native.main_window.show()
+            ui.timer(10, self.hide_window, once=True)
         else:
-            self.selection_container.set_visibility(True)
-            self.tracker_container.set_visibility(False)
+            self.selection_box.set_visibility(True)
+            self.tracker_box.set_visibility(False)
             
 
 
